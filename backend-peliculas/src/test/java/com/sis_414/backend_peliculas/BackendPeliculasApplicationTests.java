ab26796a317371ecf41ffed759ae2b924dba6269
Adriana Sullca Colque <asullca12@gmail.com>
2026-06-19 21:03:24 -0400

    estructura inicial de Spring Boot para API Peliculas

---
+++ backend-peliculas/src/test/java/com/sis_414/backend_peliculas/BackendPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sis_414.backend_peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
